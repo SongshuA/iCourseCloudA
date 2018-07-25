55878cbb87165dace875c3bd8a77fe22f1e537b3
songshu <xhskedu@qq.com>
2018-07-25 14:24:19 +0800

    写了一点注释

--- src/service/AnswerService.java
+++ src/service/AnswerService.java
@@ -7,13 +7,46 @@ import service.impl.AnswerServiceImpl;
 public interface AnswerService {
     static AnswerService getInstance() { return AnswerServiceImpl.getInstance(); }
 
+    /**
+     * 通过用户名与作业对象ID来获取该用户在该作业上的的答案
+     * @param username 用户名
+     * @param homeworkId 作业对象ID
+     * @return 答案对象
+     */
     Answer getAnswer(String username, int homeworkId);
 
+    /**
+     * 创建答案对象
+     * @param context 内容
+     * @param username 发表者用户名
+     * @param homeworkId 答案对应的作业对象ID
+     * @return 创建的答案对象ID
+     * @throws ServiceException 操作失败时抛出的异常，通过e.getMessage()获得错误原因
+     */
     int createAnswer(String context, String username, int homeworkId) throws ServiceException;
 
+    /**
+     * 通过ID获取答案对象
+     * @param id ID
+     * @return 答案对象
+     */
     Answer getAnswerById(int id);
 
+
+    /**
+     * 修改答案内容
+     * @param id 修改的答案ID
+     * @param context 内容
+     * @throws ServiceException 操作失败时抛出的异常，通过e.getMessage()获得错误原因
+     */
     void updateAnswerById(int id, String context) throws ServiceException;
 
+
+    /**
+     * 为答案进行评分（用于老师批改）
+     * @param id 答案ID
+     * @param score 分数
+     * @throws ServiceException 操作失败时抛出的异常，通过e.getMessage()获得错误原因
+     */
     void examineAnswerById(int id, int score) throws ServiceException;
 }
--- src/service/BarrageService.java
+++ src/service/BarrageService.java
@@ -9,7 +9,22 @@ import java.util.List;
 public interface BarrageService {
     static BarrageService getInstance() { return BarrageServiceImpl.getInstance(); }
 
+    /**
+     * 根据知识点ID获取所有的弹幕对象
+     * @param pointId 知识点ID
+     * @return 弹幕对象集合
+     * @throws ServiceException 操作失败时抛出的异常，通过e.getMessage()获得错误原因
+     */
     List<Barrage> getBarrages(int pointId) throws ServiceException;
 
+    /**
+     * 创建弹幕对象（用于用户创建弹幕）
+     * @param context 弹幕内容
+     * @param time 在视频中出现的时间
+     * @param username 发表者用户名
+     * @param pointId 对应的知识点ID
+     * @return 创建的弹幕对象ID
+     * @throws ServiceException 操作失败时抛出的异常，通过e.getMessage()获得错误原因
+     */
     int createBarrage(String context, int time, String username, int pointId) throws ServiceException;
 }
